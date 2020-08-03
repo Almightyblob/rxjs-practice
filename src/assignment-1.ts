9f88d12f1596d909ba87ef61858bca0a609354a3
Dominik Antunovic <dominik@dominiks-mbp.lan>
2020-08-03 21:06:03 +0200

    head explode

--- src/assignment-1.ts
+++ src/assignment-1.ts
@@ -27,8 +27,29 @@ import { map } from 'rxjs/operators';
     FizzBuzz
 */
 
-const fizzBuzz$ = interval(1000).pipe(
-    map(n => n += 1)
+const numbers$: Observable<number> = interval(1000).pipe(
+    map(n => n += 1),
 );
+
+const fizz$: Observable<string> = numbers$.pipe(
+    map(n => (n % 3 === 0) ? "Fizz" : null)
+)
+
+const buzz$: Observable<string> = numbers$.pipe(
+    map(n => (n % 5 === 0) ? "Buzz" : null)
+)
+
+const fizzBuzz$ = zip<[string, string, number]>(
+     fizz$, buzz$, numbers$
+).pipe(
+    map( arr => {
+        if ( typeof arr[0] == 'string'  || typeof arr[1] == 'string' ) {
+            return arr.slice(0, -1).join('')
+        }
+        else {
+            return arr.join('')
+        }
+    })
+)
 
 export default fizzBuzz$;
