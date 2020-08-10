7d13881aa993d63e2e5d654b6ad8817a400abb80
Dominik Antunovic <dominik@dominiks-mbp.lan>
2020-08-10 15:25:14 +0200

    reworked code, now works as intended

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,65 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 import {FizzbuzzService} from "./services/fizzbuzz.service";
-import {Observable} from "rxjs";
+import {fromEvent, Observable, of} from "rxjs";
+import {mapTo, merge, switchMap, distinctUntilChanged, tap} from "rxjs/operators";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterViewInit{
   title = 'fizzbuzz';
 
-  fizzbuzz$: Observable<string | number>;
+  fizzBuzz$: Observable<string | number>;
   result$: Observable<boolean>;
   points$: Observable<number>;
   fails$: Observable<number>;
+
+  @ViewChild('fizz', {static: true}) fizzButton: ElementRef;
+  @ViewChild('buzz', {static: true}) buzzButton: ElementRef
+  @ViewChild('fizzBuzz', {static: true}) fizzBuzzButton: ElementRef
+
+  checkTrue(value) {
+    const fizzValue = this.fizzbuzzService.fizzStream$.getValue()
+        if ((value == fizzValue && typeof fizzValue == 'string')) {
+          console.log('correct')
+            return true;
+        } else if ((value !== fizzValue && typeof fizzValue == 'string')) {
+          console.log('wrong')
+            return false;
+        } else {
+            return
+        }
+    }
 
   constructor(private fizzbuzzService: FizzbuzzService) {
   }
 
   ngOnInit() {
-    this.fizzbuzz$ = this.fizzbuzzService.fizzBuzz$;
-    this.result$ = this.fizzbuzzService.result$;
-    this.points$ = this.fizzbuzzService.points$;
-    this.fails$ = this.fizzbuzzService.fails$;
+    this.fizzBuzz$ = this.fizzbuzzService.fizzBuzz$
     this.fizzbuzzService.fizzBuzz();
   }
 
+  ngAfterViewInit() {
+    console.log(this.fizzButton)
+    const fizz = fromEvent(this.fizzButton.nativeElement, 'click')
+
+    const buzz = fromEvent(this.buzzButton.nativeElement, 'click')
+
+    const fizzBuzz = fromEvent(this.fizzBuzzButton.nativeElement, 'click')
+
+    const userInput = of().pipe(merge(
+        fizz.pipe(mapTo('Fizz')),
+        buzz.pipe(mapTo('Buzz')),
+        fizzBuzz.pipe(mapTo('FizzBuzz'))),
+        distinctUntilChanged()
+    )
+
+    const compare = this.fizzbuzzService.fizzBuzz$.pipe(switchMap(x => userInput),
+        tap(value => this.checkTrue(value))).subscribe(console.log)
+  }
+
   isNumber(val): boolean { return typeof val === 'number'; }
-
-  checkFizz(){
-    this.fizzbuzzService.checkFizz('Fizz');
-  }
-
-  checkBuzz(){
-    this.fizzbuzzService.checkFizz('Buzz')
-  }
-
-  checkFizzBuzz(){
-    this.fizzbuzzService.checkFizz('FizzBuzz')
-  }
 
 }
--- src/app/services/fizzbuzz.service.ts
+++ src/app/services/fizzbuzz.service.ts
@@ -1,21 +1,13 @@
-import {BehaviorSubject, combineLatest, interval, Observable, zip} from "rxjs";
-import {map, tap} from "rxjs/operators";
+import {BehaviorSubject, interval, Observable, zip} from "rxjs";
+import {map} from "rxjs/operators";
 
 export class FizzbuzzService {
 
-    fizzStream$ = new BehaviorSubject<string | number>('');
-    fizzBuzz$ = this.fizzStream$.asObservable();
-    bet$ = new BehaviorSubject<string>('')
-    comparedValues$ = new BehaviorSubject<[string | number, string]>([null, null])
-    result$ = new BehaviorSubject<boolean>(null);
-    points$ = new BehaviorSubject<number>(0)
-    points = 0;
-    fails$ = new BehaviorSubject<number>(0)
-    fails = 0;
+    fizzStream$ = new BehaviorSubject<string | number>(null);
+    fizzBuzz$ = this.fizzStream$.asObservable()
 
     numbers$: Observable<number> = interval(2000).pipe(
         map(n => n += 1),
-        tap(n => this.result$.next(null))
     );
 
     fizz$: Observable<string> = this.numbers$.pipe(
@@ -26,35 +18,24 @@ export class FizzbuzzService {
         map(n => (n % 5 === 0) ? "Buzz" : null)
     )
 
-     fizzBuzz() { zip<[string, string, number]>(
-        this.fizz$, this.buzz$, this.numbers$
-    ).pipe(
-        map( arr => (arr.slice(0,2).some(Boolean)) ?
-            arr.slice(0, -1).join('') : +arr.join((''))
-        )
-    ).subscribe( fb => this.fizzStream$.next(fb))
-     }
+    fizzBuzz() {
+        zip<[string, string, number]>(
+            this.fizz$, this.buzz$, this.numbers$
+        ).pipe(
+            map(arr => (arr.slice(0, 2).some(Boolean)) ?
+                arr.slice(0, -1).join('') : +arr.join((''))
+            )
+        ).subscribe(fb => this.fizzStream$.next(fb))
+    }
 
-    betVScurrent$ = combineLatest([this.fizzBuzz$, this.bet$])
-        .subscribe( result => this.comparedValues$.next(result))
-
-     checkTrue(arr) {
-        if ((arr[0] === arr[1] && typeof arr[0] == 'string')) {
-            this.points++;
-            this.points$.next(this.points);
-            return true;
-        } else if ((arr[0] !== arr[1] && typeof arr[0] == 'string')){
-            this.fails++;
-            this.fails$.next(this.fails);
-            return false;
-        } else {
-            return
-        }
-     }
-
-     checkFizz(fizzOrBuzz) {
-        this.bet$.next(fizzOrBuzz)
-         console.log(this.comparedValues$)
-         this.checkTrue(this.comparedValues$.getValue())
-     }
+    // checkTrue(arr) {
+    //     if ((arr[0] === arr[1] && typeof arr[0] == 'string')) {
+    //         return true;
+    //     } else if ((arr[0] !== arr[1] && typeof arr[0] == 'string')) {
+    //         return false;
+    //     } else {
+    //         return
+    //     }
+    // }
 }
+
